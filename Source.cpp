b99c81f64010d09d5a2a26e47b790658e62ce2eb
Nicholas Khouri <magickhourdini@gmail.com>
2017-12-02 17:05:25 -0500

    Fixed the weapon generator and deleted the old item generator. Added noun and adjetive text files

--- Source.cpp
+++ Source.cpp
@@ -1,37 +1,26 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <ctime>
 #include "Player.h"
 
 
 using namespace std;
 
 void goToWild(Player player);
+Weapon generateWeapon(int);
+string generateName();
 //void generateLoot(Player player);
 int nounCount = 0;
 int adjetiveCount = 0;
-string noun[1000];
-string adjetive[1000];
 int main() {
 	string nameInput; // needed to take name from console
 	Player player = Player(); // set the starting character at the begining of the game
 	bool isPlaying = true;
 	ifstream reader;
 
-	//PRE GAME GEREATION
-	reader.open("nouns.txt");
-
-	while (!reader.eof()) {
-		reader >> noun[nounCount];
-		nounCount++;
-	}
-	reader.close();
-	reader.open("adjetives.txt");
-	while (!reader.eof()) {
-		reader >> adjetive[adjetiveCount];
-		adjetiveCount++;
-	}
-	reader.close();
+	//testing environment
+	//END TEST ENVIRONMENT
 
 
 	cout << "Welcome, warrior!\n";
@@ -79,7 +68,6 @@ void goToWild(Player player)
 	int chance = rand() % 100;
 	if (chance > 90) {
 		cout << "You wander around the land and find a chest full of loot!\n";
-		//generateLoot(player);
 	}
 	else {
 		cout << "An Enemy is approaching...\n";
@@ -87,67 +75,114 @@ void goToWild(Player player)
 	}
 }
 
-//***************************************
-//		LOOT GENERATOR
-//	Used to generate a random set of loot for the player to take
-//	Parameter: Player who is receiving the loot
-//***************************************
-/*
-void generateLoot(Player player)
+//Generate a new weapon
+Weapon generateWeapon(int c) //c is player level
 {
-	//have a maximum of 2 items of 1 type to loot at 1 time
-	//gen weapons
-	int luck = rand() % 2; //make the user either get 0, 1, or 2 items
-	for (int i = 0; i < luck; i++) {
-		Weapon genW = Weapon();		//holds the wepon to generate attributes
-		int luckp = rand() % 4;		//gives more damage to a weapon with this roll
-		int randA = rand() % adjetiveCount;
-		int randN = rand() % nounCount;
-		string name = adjetive[randA] + " " + noun[randN];
-		genW.setName(name);
-		luck = rand() % (player.getLevel() + 3) + (player.getLevel() - 3); //sets the level of the sword
-		if (luck <= 0)	//this catches an error that would cause the sword to do negative damage if the player is a low level
-			luck = 0;
-		genW.setLevel(luck);
-		genW.setDamage(luckp + player.getLevel());
-		player.addBackpack(genW, player.getWeaponCount(player.getBackpack), player.getBackpack); //adding the generated weapon into the backpack
+	srand(time(NULL)); //set the new seed for the generation
+	
+	Weapon weapon;
+	string noun[100]; //hold a max of 100 nouns
+	string adjetive[100]; //hold a max of 100 adjetives
+	ifstream input;
+	input.open("noun.txt");
+	int nCount = 0;
+	while (!input.eof()) {
+		input >> noun[nCount];
+		nCount++;
 	}
-	int luck = rand() % 2; //next, the armor
-	for (int i = 0; i < luck; i++) {
-		Armor genA = Armor();	//holds armor to generate attributes
-		int luckp = rand() % 2;		//gives extra armor on top of the level
-		int randA = rand() % adjetiveCount;
-		int randN = rand() % nounCount;
-		string name = adjetive[randA] + " " + noun[randN];
-		genA.setName(name);
-		luck = rand() % (player.getLevel() + 3) + (player.getLevel() - 3);
-		if (luck <= 0)	//this catches an error that would cause the sword to do negative damage if the player is a low level
-			luck = 0;
-		genA.setLevel(luck);
-		genA.setArmorRating(luckp + player.getLevel());
-		player.addBackpack(genA, player.getArmorCount(player.getBackpack()), player.getBackpack());
+	input.close();
+	input.open("adjetive.txt");
+	int aCount = 0;
+	while (!input.eof()) {
+		input >> adjetive[aCount];
+		aCount++;
 	}
-	int luck = rand() % 3; //last, the potion
-	for (int i = 0; i < luck; i++) {
-		Potion genP = Potion();
-		luck = rand() % 3;
-		if (luck == 0) {
-			genP.setName("Small Healing Potion");
-			genP.setHealingPower(10);
-		}
-		else if (luck = 1) {
-			genP.setName("Medium Healing Potion");
-			genP.setHealingPower(20);
-		}
-		else if (luck = 2) {
-			genP.setName("Large Healing Potion");
-			genP.setHealingPower(30);
-		}
-		else if (luck = 3) {
-			genP.setName("Epic Healing Potion");
-			genP.setHealingPower(40);
-		}
-		player.addBackpack(genP, player.getPotionCount(player.getBackpack()), player.getBackpack());
+	string nameNoun = noun[(rand() % nCount)];
+	string nameAdjetive = adjetive[(rand() % aCount)];
+	//store the name into the weapon
+	string name = nameAdjetive + " " + nameNoun;
+	weapon.setName(name);
+	//set the level based on the players level
+	if (c <= 5) {
+		weapon.setLevel(1);
 	}
+	else {
+		int level = (rand() % c) + 5;
+		weapon.setLevel(level);
+	}
+	//set the damage based on the players level
+	//added set multipliers to damage to have a better loot outcome based of player level;
+
+	int damage = (rand() % c) + 5;
+	if (c < 15) {
+		damage = damage * 1.2; //20% inclrease base
+	}
+	else if (c >= 15 && c < 30) {
+		damage = damage * 1.6; //60% increase at lvl 15
+	}
+	else {
+		damage = damage * 2; //100% increase at 30+
+	}
+	weapon.setDamage(damage);
+	return weapon;
 }
-*/
+
+//Manually generate damage
+int generateDamage(int c) {
+	srand(time(NULL)); //set the new seed for the generation
+
+	int damage = (rand() % c) + 5;
+	if (c < 15) {
+		damage = damage * 1.2; //20% inclrease base
+	}
+	else if (c >= 15 && c < 30) {
+		damage = damage * 1.6; //60% increase at lvl 15
+	}
+	else {
+		damage = damage * 2; //100% increase at 30+
+	}
+
+	return damage;
+}
+
+//Manually generate a level
+int generateLevel(int c) {
+	int level;
+	if (c <= 5) {
+		level = 1;
+	}
+	else {
+		level = (rand() % c) + 5;
+	}
+
+	return level;
+}
+
+//Manually generate a name
+string generateName() {
+	srand(time(NULL)); //seeds the number generators
+
+	string noun[100]; //hold a max of 100 nouns
+	string adjetive[100]; //hold a max of 100 adjetives
+	ifstream input;
+	input.open("noun.txt");
+	int nCount = 0;
+	while (!input.eof()) {
+		input >> noun[nCount];
+		nCount++;
+	}
+	input.close();
+	input.open("adjetive.txt");
+	int aCount = 0;
+	while (!input.eof()) {
+		input >> adjetive[aCount];
+		aCount++;
+	}
+	string nameNoun = noun[(rand() % nCount)];
+	string nameAdjetive = adjetive[(rand() % aCount)];
+	//store the name into the weapon
+	string name = nameAdjetive + " " + nameNoun;
+	cout << name << endl;
+
+	return name;
+}
--- Weapon.cpp
+++ Weapon.cpp
@@ -21,7 +21,7 @@ void Weapon::setName(std::string n)
 
 std::string Weapon::getName()
 {
-	return std::string();
+	return name;
 }
 
 void Weapon::setLevel(int l)
@@ -42,51 +42,4 @@ void Weapon::setDamage(int d)
 int Weapon::getDamage()
 {
 	return damage;
-}
-
-Weapon Weapon::generateWeapon(int c) //c is player level
-{
-	std::string noun[100]; //hold a max of 100 nouns
-	std::string adjetive[100]; //hold a max of 100 adjetives
-	std::ifstream input;
-	input.open("noun.txt");
-	int nCount = 0;
-	while (!input.eof()) {
-		input >> noun[nCount];
-	}
-	input.close();
-	input.open("adjetive.txt");
-	int aCount = 0;
-	while (!input.eof()) {
-		input >> adjetive[aCount];
-	}
-	Weapon w = Weapon();
-	std::string nameNoun = noun[(rand() % nCount) + 1];
-	std::string nameAdjetive = adjetive[(rand() % aCount) + 1];
-	//store the name into the weapon
-	w.setName(nameNoun + " " + nameAdjetive);
-	//set the level based on the players level
-	if (c > 5) {
-		w.setLevel(1);
-	}
-	else {
-		int level = (rand() % c) + 5;
-		w.setLevel(level);
-	}
-	//set the damage based on the players level
-	//added set multipliers to damage to have a better loot outcome based of player level;
-
-	int damage = (rand() % c) + 5;
-	if (c < 15) {
-		damage = damage * 1.2; //20% inclrease base
-	}
-	else if (c >= 15 && c < 30) {
-		damage = damage * 1.6; //60% increase at lvl 15
-	}
-	else {
-		damage = damage * 2; //100% increase at 30+
-	}
-	w.setDamage(damage);
-
-	return w;
 }
